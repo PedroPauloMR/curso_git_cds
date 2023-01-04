67632a632f073e3fed36a27f98b4395dabd6f0ba
PedroPauloMR <pedrop.ribeiro@hotmail.com>
2023-01-04 13:07:43 -0300

    criada funcao de carga dentro de src\app.py

--- src/app.py
+++ src/app.py
@@ -3,12 +3,16 @@ import pandas as pd
 import numpy as np
 
 
+def load():
+    print('realiza a carga dos dados dentro do banco de dados...')
+    return None
 
+    
 def transformation():
     print('realiza a transformação dos dados...')
     return None
 
-    
+
 def extraction():
     print('realiza a extração do dado...')
     return None
