1d3611716679db8489953eb0e3e6513f2fe92e5c
PedroPauloMR <pedrop.ribeiro@hotmail.com>
2023-01-04 13:05:56 -0300

    criada funcao de extracao dentro de src\app.py

--- src/app.py
+++ src/app.py
@@ -2,8 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+
+def extraction():
+    print('realiza a extração do dado...')
+    return None
+
+
 def main():
     print('hello world')
+    return None
+
+    
 
 if __name__ == '__main__':
     main()
